f0ca6022967d7e26f987f7ccf24bd6b3a09b4ded
GitHub Action <action@github.com>
2025-07-15 11:59:49 +0000

    ci: Update Flutter Web build artifacts
    - Built from commit: 60f6d55a97ec584b3f00060008906f06ae81ce67
    - Branch: main
    - Workflow: Build and Deploy Flutter Web
    
    Auto-generated by GitHub Actions

--- kunhavigi_server/web/static/flutter_service_worker.js
+++ kunhavigi_server/web/static/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c9fb1e4dc17c601e74fec046218848e5
 "assets/packages/gpt_markdown/lib/fonts/JetBrainsMono-Regular.ttf": "d09f65145228b709a10fa0a06d522d89",
 "version.json": "6f46c28e88fe30c16d43111e1bc0bccf",
 "manifest.json": "69ca1c2bbbfdc6512220a99a171d9c13",
-"flutter_bootstrap.js": "3a1340074f02811e769b44e52a85be99",
+"flutter_bootstrap.js": "fd53ebc7ed4e0022ffb4a02e5af4e76c",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
@@ -52,7 +52,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c9fb1e4dc17c601e74fec046218848e5
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"main.dart.js": "68151fca31fed7c1ec06ba9f6d410647"};
+"main.dart.js": "d0f083e73683f2ea84e931aa6f3a8ca7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
